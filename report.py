4b025d4c3f2595401defef9bfbda5e429c40733d
Nandor Licker <n@ndor.email>
2020-11-02 16:56:13 +0000

    [llir-benchmark] Report works with a single run

--- report.py
+++ report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import json
 import textwrap
@@ -10,6 +10,15 @@ import scipy.stats
 with open('_result/macro', 'r') as f:
   data = json.loads(f.read())
 
+def independent(t0, t1):
+  if len(t0) < 2 or len(t1) < 2:
+    return True
+  _, p = scipy.stats.ttest_ind(t0, t1)
+  return p < 0.01
+
+def stdev(ts):
+  if len(ts) < 2: return 0.0
+  return statistics.stdev(ts)
 
 for bench, runs in data.items():
   def get_times(key): return [t for t, _ in runs[key]]
@@ -18,20 +27,22 @@ for bench, runs in data.items():
   times = ''
   ref_times = get_times('ref')
   ref_mean = statistics.mean(ref_times)
-  times += '{:6.2f}/{:5.3f}'.format(ref_mean, statistics.stdev(ref_times))
+  times += '{:6.2f}/{:5.3f}'.format(ref_mean, stdev(ref_times))
 
   for key in keys:
     key_times = get_times(key)
     dev_mean = statistics.mean(key_times)
-    times += '   {:6.2f}/{:5.3f}'.format(dev_mean, statistics.stdev(key_times))
+    times += '   {:6.2f}/{:5.3f}'.format(dev_mean, stdev(key_times))
 
-    _, p = scipy.stats.ttest_ind(ref_times, key_times)
-    if p < 0.01:
-      f = dev_mean / ref_mean * 100 - 100
-      times += '{:>8}'.format('{:+5.2f}'.format(f))
+    if independent(ref_times, key_times):
+      if ref_mean == 0.0:
+        times += '        '
+      else:
+        f = dev_mean / ref_mean * 100 - 100
+        times += '{:>8}'.format('{:+5.2f}'.format(f))
     else:
       times += ' ' * 8
-  
+
   if len(bench) > 27:
     bench = bench[:27] + '...'
   print('{} {}'.format(bench.ljust(30), times))
