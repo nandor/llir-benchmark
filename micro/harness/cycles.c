f9cca40c60d11dca24c69f458ebb68d4e73afcfa
Nandor Licker <n@ndor.email>
2020-11-10 19:24:17 +0000

    [llir-benchmark] Disabled readcyclecounter on AArch64

--- micro/harness/cycles.c
+++ micro/harness/cycles.c
@@ -1,6 +1,13 @@
 #include <caml/mlvalues.h>
+#include <sys/time.h>
 
 CAMLprim value ocaml_read_time_stamp_counter()
 {
-  return Val_long( __builtin_readcyclecounter() );
+#ifdef __x86_64__
+  return Val_long( __builtin_rdtsc() );
+#else
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (long long)(tv.tv_sec) * 1000000ll + tv.tv_usec;
+#endif
 }
