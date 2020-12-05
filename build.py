fbc6546291a0e8981c4614ba5de1ce9056c4588e
Nandor Licker <n@ndor.email>
2020-12-05 16:01:44 +0000

    [llir-benchmark] Added mirage switches

--- build.py
+++ build.py
@@ -22,36 +22,90 @@ CPUS = {
   'power': ['pwr8', 'pwr9']
 }
 
+# Enumeration of opt levels.
+OPT = ['O0', 'O1', 'O2', 'O3', 'O4']
+
+# List of all packages to install.
+CORE_PACKAGES=[
+  "alcotest", "alt-ergo-free", "angstrom", "atdgen", "atdj", "atds", "base",
+  "base64", "bheap", "bigstringaf", "biniou", "bos", "camlp4", "camlp5",
+  "camlzip", "cmitomli", "compcert", "coq", "cpdf", "cppo", "crowbar", "csexp",
+  "cstruct", "cstruct-sexp", "cstruct-unix", "cubicle", "digestif", "diy",
+  "domain-name", "dune", "duration", "easy-format", "eqaf", "fix", "frama-c",
+  "fraplib", "gmp", "hevea", "hxd", "io-page", "ipaddr", "irmin", "irmin-mem",
+  "irmin-test", "js_of_ocaml", "jsonm", "lru", "lwt", "lwt-dllist", "macaddr",
+  "menhir", "metrics-unix", "minilight", "nbcodec", "num", "ocaml-syntax-shims",
+  "ocamlformat", "ocamlgraph", "ocamlmod", "ocplib-endian", "ocplib-simplex",
+  "odoc", "ounit2", "pcap-format", "ppx_cstruct", "ppx_deriving", "ppx_tools",
+  "ppxfind", "ppxlib", "ptime", "randomconv", "re", "react", "reanalyze",
+  "reason", "result", "rml", "sexplib", "sexplib0", "stdio", "stringext",
+  "tcpip","tuntap", "tyxml", "ucaml", "uri", "uucp", "uuidm", "why3",
+  "xenstore", "yojson", "zarith", "zlib",
+]
+
+# List of mirage-specific packages.
+MIRAGE_PACKAGES=[
+  "alcotest", "arp", "asn1-combinators", "astring", "benchmark", "bheap",
+  "bigarray-compat", "charrua", "charrua-server", "charrua-unix",
+  "conduit", "conduit-mirage", "conf-pkg-config", "cstruct", "crunch",
+  "cstruct-sexp", "cstruct-unix", "diet", "dns", "dns-client", "domain-name",
+  "dune", "dune-configurator", "duration", "eqaf", "ethernet", "fiat-p256",
+  "fmt", "functoria", "functoria-runtime", "hex", "hkdf", "io-page", "ipaddr",
+  "ipaddr-cstruct", "ipaddr-sexp", "ke", "libseccomp", "logs", "lru", "lwt",
+  "lwt-dllist", "macaddr", "macaddr-cstruct", "macaddr-sexp", "magic-mime",
+  "mirage", "mirage-block", "mirage-block-unix", "mirage-bootvar-unix",
+  "mirage-channel", "mirage-clock", "mirage-clock-unix", "mirage-console",
+  "mirage-console-unix", "mirage-crypto", "mirage-device", "mirage-flow",
+  "mirage-fs", "mirage-kv", "mirage-kv-mem", "mirage-logs", "mirage-net", 
+  "mirage-net-unix", "mirage-profile", "mirage-protocols", "mirage-random", 
+  "mirage-random-test", "mirage-runtime", "mirage-stack", "mirage-time", 
+  "mirage-time-unix", "mirage-types", "mirage-types-lwt", "mirage-vnetif", 
+  "opam-depext", "ounit", "parse-argv", "pcap-format", "ppx_cstruct", "ptime", 
+  "randomconv", "rresult", "sexplib", "shared-memory-ring", 
+  "shared-memory-ring-lwt", "stdlib-shims", "tcpip", "tuntap", "vchan", 
+  "xenstore", "yojson"
+]
+
 # Switches with root packages.
 SWITCHES = {}
+PACKAGES = {}
+
 for arch, cpus in CPUS.items():
   SWITCHES[f'{arch}+ref'] = [f'ocaml-variants-{arch}.4.11.1.master']
+  PACKAGES[f'{arch}+ref'] = CORE_PACKAGES
   SWITCHES[f'{arch}+llir'] = [f'ocaml-variants-{arch}.4.11.1.master+llir']
-  for opt in ['O0', 'O1', 'O2', 'O3', 'O4']:
+  PACKAGES[f'{arch}+llir'] = CORE_PACKAGES
+  for opt in OPT:
     SWITCHES[f'{arch}+llir+{opt}'] = [
         f'ocaml-variants-{arch}.4.11.1.master+llir',
         f'llir-config.{opt}'
     ]
+    PACKAGES[f'{arch}+llir+{opt}'] = CORE_PACKAGES
     for cpu in cpus:
       SWITCHES[f'{arch}+llir+{opt}+{cpu}'] = [
           f'ocaml-variants-{arch}.4.11.1.master+llir',
           f'llir-config.{opt}+{cpu}'
       ]
 
-# List of all packages to install.
-PACKAGES=[
-  "coq", "menhir", "compcert", "ocamlgraph", "cpdf", "minilight", "yojson",
-  "base", "stdio", "dune", "camlzip", "zarith", "ocplib-simplex", "diy",
-  "ocamlmod", "sexplib0", "odoc", "hevea", "cmitomli", "alcotest", "biniou",
-  "bigstringaf", "result", "angstrom", "hxd", "cppo", "csexp", "easy-format",
-  "gmp", "ocaml-syntax-shims", "ounit2", "ppxlib", "re", "zlib", "jsonm",
-  "uuidm", "ocplib-endian", "lwt", "fix", "why3", "tyxml", "nbcodec", "react",
-  "rml", "uucp", "atdgen", "atdj", "atds", "camlp5", "ppxfind", "ucaml",
-  "ocamlformat", "js_of_ocaml", "ppx_deriving", "ppx_tools", "frama-c",
-  "alt-ergo-free", "camlp4", "num", "fraplib", "cubicle", "uri", "irmin",
-  "irmin-mem", "irmin-test", "stringext", "bos", "base64", "digestif",
-  "eqaf", "crowbar", "metrics-unix", "reason", "reanalyze"
-]
+for arch in ['x86_64']:
+  SWITCHES[f'{arch}+mirage+ref'] = [
+    f'ocaml-variants-{arch}.4.11.1.master'
+  ]
+  PACKAGES[f'{arch}+mirage+ref'] = MIRAGE_PACKAGES
+
+  SWITCHES[f'{arch}+mirage+llir'] = [
+    f'ocaml-variants-{arch}.4.11.1.master+llir'
+  ]
+  PACKAGES[f'{arch}+mirage+llir'] = MIRAGE_PACKAGES
+
+  for opt in OPT:
+    SWITCHES[f'{arch}+mirage+llir+{opt}'] = [
+      f'ocaml-variants-{arch}.4.11.1.master+llir',
+      f'llir-config.{opt}'
+    ]
+    PACKAGES[f'{arch}+mirage+llir+{opt}'] = MIRAGE_PACKAGES
+
+
 
 def opam(args, capture=False, silent=False, cwd=None, **kwargs):
   """Run the opam process and capture its output."""
@@ -149,7 +203,7 @@ def install(switches, repository, jb, test):
           '--switch={}'.format(switch),
           '--yes',
           '-j', str(jb),
-        ] + (['--with-test'] if test else []) + PACKAGES,
+        ] + (['--with-test'] if test else []) + PACKAGES[switch],
         prefix=os.path.join(OPAMROOT, switch)
     )
 
